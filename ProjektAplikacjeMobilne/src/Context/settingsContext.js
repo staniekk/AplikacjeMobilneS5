8ed2d298ad081b7874ccb259cd8b41dcc249b6fa
JustMax <k.szwedkowicz01@gmail.com>
2024-01-21 18:07:41 +0100

    Settings are now downloaded from db

--- ProjektAplikacjeMobilne/src/Context/settingsContext.js
+++ ProjektAplikacjeMobilne/src/Context/settingsContext.js
@@ -1,4 +1,5 @@
 import { func } from 'prop-types';
+import axios from 'axios';
 import React, { createContext, useState, useEffect, useContext } from 'react';
 
 export const SettingsContext = createContext({
@@ -14,6 +15,31 @@ export default function SettingsContextProvider({ children }) {
   const [dailyStepGoal, setDailyStepGoal] = useState(10000);
 
 
+  useEffect( () => {
+
+    const getSettings = async () => {
+        axios.get('https://65ad4acaadbd5aa31be0832b.mockapi.io/am/userSettings')
+    .then(response => {
+        const userSettings = response.data;
+        const foundSettings = userSettings.find(settings => settings.userID === userID);
+        if (foundSettings) {
+            console.log('Settings step goal:' +  foundSettings.dailyStepGoal);
+            console.log('Authenticated user:' +  foundSettings);
+        } else {
+            Alert.alert("Error", "Settings not found");
+        }
+    })
+    .catch(error => {
+        Alert.alert("Login Error", error.message);
+    });
+    }
+
+    
+    if(userID !== -1){
+        getSettings();
+    }
+
+  }, [userID]);
 
   return (
     <SettingsContext.Provider 
