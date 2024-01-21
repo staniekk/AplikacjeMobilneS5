71753fdf5d7959407c6bf393bd27f13ae71d6d5b
JustMax <k.szwedkowicz01@gmail.com>
2024-01-21 23:33:25 +0100

    Fixes null history error

--- ProjektAplikacjeMobilne/src/views/HistoryS/index.js
+++ ProjektAplikacjeMobilne/src/views/HistoryS/index.js
@@ -8,15 +8,18 @@ import { SettingsContext } from '../../Context/settingsContext';
 const HistoryS = () => {
   const [history, setHistory] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [hasHistory, setHasHistory] = useState(false);
   const {userID} = useContext(SettingsContext)
   //Pobieranie danych z bazy danych
   useEffect(() => {
     axios.get(`https://65a40329a54d8e805ed451eb.mockapi.io/api/am/history?userID=${userID}`)
       .then(response => {
+        setHasHistory(true);
         setHistory(response.data);
         setLoading(false);
       })
       .catch(error => {
+        setHasHistory(false);
         console.error("Wystąpił błąd podczas pobierania danych historii", error);
         console.error("ID" + userID);
         setLoading(false);
@@ -28,30 +31,46 @@ const HistoryS = () => {
     return <ActivityIndicator size="large" color="#0000ff" />;
   }
 
+
+  const content = hasHistory ? (
+    <ScrollView style={styles.mainContainer}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Image style={styles.logo} source={require('../../img/logo/Logo.png')}/>
+   {history.map((entry) => (
+       <View key={entry.id} style={styles.historyInfo}>
+           <View key={entry.id} style={styles.historyInfo2}>
+           <Image style={styles.runner} source={require('../../img/logo/runner.png')}/>
+           <View style={{flex: 1}}>
+           <Text style={styles.historyInfoText}>
+             {entry.dist} km - {entry.date} 
+           </Text>
+           <Text style={styles.historyInfoText2}>
+             Czas: {entry.time} h - Kroki: {entry.steps}
+           </Text>
+           </View>
+       </View>
+     </View>
+   ))}
+   </View>
+ </ScrollView>
+  ) : 
+  (
+    <View>
+        <Text style={styles.historyInfo}>
+          You have no history
+        </Text>
+    </View>
+  );
+
   return (
-   
+
+    <View>
+      {content}
+
+    </View>
     //Wyświetla wszystkie znalezione rekordy z bazy danych
     //Dla każdego rekordu wyświetla jego dane
-    <ScrollView style={styles.mainContainer}>
-       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-       <Image style={styles.logo} source={require('../../img/logo/Logo.png')}/>
-      {history && history.map((entry) => (
-        <View key={entry.id} style={styles.historyInfo}>
-          <View key={entry.id} style={styles.historyInfo2}>
-          <Image style={styles.runner} source={require('../../img/logo/runner.png')}/>
-          <View style={{flex: 1}}>
-          <Text style={styles.historyInfoText}>
-            {entry.dist} km - {entry.date} 
-          </Text>
-          <Text style={styles.historyInfoText2}>
-            Czas: {entry.time} h - Kroki: {entry.steps}
-          </Text>
-        </View>
-        </View>
-        </View>
-      ))}
-      </View>
-    </ScrollView>
+   
     
   );
 };
